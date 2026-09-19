ec666ca64a741033937dc1b017d2b7826c414ba0
Ab1805 <abolii.adhalrao1254@gmail.com>
2026-09-18 19:53:33 -0500

    Add meteor environment

---
+++ Assets/BreakableAsteroids/Scripts/Fracture.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Fracture : MonoBehaviour
+{
+    [Tooltip("The broken version of this asteroid.")]
+    public GameObject fractured;
+
+    public bool breakAutomatically = true;
+
+    [Tooltip("World height of the ground surface.")]
+    public float groundHeight = -5f;
+
+    private Collider rockCollider;
+    private bool hasBroken;
+
+    private void Awake()
+    {
+        rockCollider = GetComponent<Collider>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!breakAutomatically || hasBroken)
+            return;
+
+        float bottom = rockCollider != null
+            ? rockCollider.bounds.min.y
+            : transform.position.y;
+
+        if (bottom <= groundHeight)
+            FractureObject();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (breakAutomatically)
+            FractureObject();
+    }
+
+    public void FractureObject()
+    {
+        if (hasBroken || fractured == null)
+            return;
+
+        hasBroken = true;
+        Instantiate(fractured, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
